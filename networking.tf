59790fd2c2440b96201ea95cf2e71767882d8caf
wdtorres <wdtorres@yahoo.com>
2024-02-22 11:07:44 -0500

    quedo mi infra muy bonita

--- networking.tf
+++ networking.tf
@@ -1,46 +1,46 @@
 # Create a VPC
-resource "aws_vpc" "VPC-DTU" {
+resource "aws_vpc" "vpc-dtu" {
   cidr_block = "10.0.0.0/27"
   tags = {
     Name = "VPC-DTU"
   }
 }
-resource "aws_subnet" "SUB-EXT" {
-  vpc_id     = aws_vpc.VPC-DTU.id
+resource "aws_subnet" "sub-exxt" {
+  vpc_id     = aws_vpc.vpc-dtu.id
   cidr_block = "10.0.0.0/28"
 
   tags = {
     Name = "Internet"
   }
 }
-resource "aws_subnet" "SUB-INT" {
-  vpc_id     = aws_vpc.VPC-DTU.id
+resource "aws_subnet" "sub-int" {
+  vpc_id     = aws_vpc.vpc-dtu.id
   cidr_block = "10.0.0.16/28"
 
   tags = {
     Name = "Intranet"
   }
 }
-resource "aws_internet_gateway" "GTWY" {
-  vpc_id = aws_vpc.VPC-DTU.id
+resource "aws_internet_gateway" "gtwyintr" {
+  vpc_id = aws_vpc.vpc-dtu.id
 
   tags = {
     Name = "Gateway Principal"
   }
 }
-resource "aws_route_table" "TBL-EXTERNA" {
-  vpc_id = aws_vpc.VPC-DTU.id
+resource "aws_route_table" "tbl-externa" {
+  vpc_id = aws_vpc.vpc-dtu.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.GTWY.id
+    gateway_id = aws_internet_gateway.gtwyintr.id
   }
 
   tags = {
     Name = "Tabla rutas Externa"
   }
 }
-resource "aws_route_table_association" "ASOCIA-TBL-EXT" {
-  subnet_id      = aws_subnet.SUB-EXT.id
-  route_table_id = aws_route_table.TBL-EXTERNA.id
+resource "aws_route_table_association" "asocia-tbl-extrna" {
+  subnet_id      = aws_subnet.sub-exxt.id
+  route_table_id = aws_route_table.tbl-externa.id
 }
